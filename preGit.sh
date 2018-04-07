4ad233e36afa5293ea0a664ef6628ab8204c83f1
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-04-07 15:18:09 -0400

    Travis Update

---
+++ preGit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Commands to remove files:
+# git rm --cached src/environments/environment.ts
+# git rm --cached src/environments/environment.prod.ts
+
+# Commands to encrypt files:
+# travis encrypt-file src/environments/environment.ts --add
+# travis encrypt-file src/environments/environment.prod.ts --add
+
+# Testing
+
+ng test
+
+
